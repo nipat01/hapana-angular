278dcaf67f14740cfa43c7c015ac6e1e32c0b985
1 <2>
2021-10-04 18:09:55 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,64 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'hapana';
+  character = "";
+  characters = ["a", "b", "c"];
+  chat = [
+    {
+      character: "",
+      conversation: ""
+    },
+    {
+      character: "",
+      conversation: ""
+    },
+  ];
+  titleName = "";
+  chatLists: any = [];
+
+  addCharacter() {
+    if (this.character) {
+      this.characters.push(this.character);
+      this.character = ""
+    }
+
+  }
+
+
+  saveChat() {
+    console.log("chat =>", this.chat);
+    this.chatLists.push({
+      chat: this.chat,
+      titleName: this.titleName
+    });
+
+    this.chat = [
+      {
+        character: "",
+        conversation: ""
+      },
+      {
+        character: "",
+        conversation: ""
+      },
+    ];
+    this.titleName = "";
+  }
+
+  addChat() {
+    this.chat.push({
+      character: "",
+      conversation: ""
+    })
+  }
+
+  deleteChat(index: number) {
+    console.log(index);
+    if (index > -1) {
+      this.chat.splice(index, 1);
+    }
+    console.log("chat =>", this.chat);
+  }
+
+
 }
